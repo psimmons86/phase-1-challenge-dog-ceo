82efb20ce684c08ca954169537611ba18ea4d1f3
Patrick Simmons <hadroncollides@gmail.com>
2021-03-22 14:22:38 -0400

    first run

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log('%c HI', 'color: firebrick')
+
+
+//const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+
+fetch("https://dog.ceo/api/breeds/image/random/4", {} )
+.then (res => res.json(())
+.then(function(dogResponse)) {
+
+
+}
